b34b577a8fc75619d043ecd4b8183e64e84bfb5e
Dashaway <hdtxun@gmail.com>
2019-02-14 11:44:40 +0800

    公式录入结束，未检查 准备进行阶段选择

--- burner_DA.m
+++ burner_DA.m
@@ -3,13 +3,13 @@
 %燃烧参数计算
 
 
-%190117
-%增加注释
-%换为圆柱药柱，药柱数量增加
-
 %190212
 %换为跑道型内孔
 %更改参数
+
+%190213
+%更改圆弧角度
+
 
 clear;
 close all;
@@ -22,11 +22,12 @@ dt = 5e-5;      %步进值
 %已知常量
 p0 = 1.02e5;    %初始压强(Pa)
 Dr = 0.150;       %燃烧室外径(m)
+D = Dr;         %药柱外径(m)
 r = 0.003;        %微圆弧(m)
 l = 0.055;        %中心距(m)
 R = 0.018;       %中心孔(m)
-
-n_s = 8;    %药柱数量
+m_s = 2;        %孔药比
+n_s = 4;    %药柱数量
 Lp = 0.22;      %装药长度(m)
 gamma = 1.2;        %比热比
 rho_p = 1730;       %密度(kg/m^3)
@@ -42,27 +43,42 @@ phi_m = 1;      %?
 
 Gamma = ( (2 / (gamma + 1))^( (gamma + 1) / (2*(gamma - 1)) ) ) ...
     *sqrt(gamma);      %比热比函数
+trd = (2*pi) / (n_s*(m_s + 1));     %药圆心角
+trr = (2*pi*m_s) / (n_s*(m_s + 1));     %弧圆心角
 %判别条件
-e_a = l*sqrt((1 - cos(pi / n_s)) / 2) - r;       %第一阶段
-e_b = Dr/2 - r - l;
-e_c = (l - r - R)/2;
+e_a = l*sin(trd/2) - r;       %第一阶段
+e_b = l*sin(trr/2) - r;
+e_c = D/2 - r - l;
+e_d = (l - r - R)/2;
+e_e = (D/2)*(sin(trd) / sin(trd + as2)) - r;
+
 %计算中间值
-%as =  asin( (l*sin(pi / (2*n_s))) / (r + e) );
-%ac1 = acos( (4*(r + e)^2 + 4*l^2 - Dr^2) / (8*l*(r + e)) );
-%ac2 = acos( (4*l^2 + Dr^2 - 4*(r + e)^2) / 4*l*Dr ); 
+as1 =  asin((D/2 - l) / (r + e));
+as2 =  asin( (l*sin(pi / (2*n_s))) / (r + e) );
+ac1 = acos( (4*l^2 + D^2 - 4*(r + e)^2) / 4*l*D ); 
+ac2 = acos( (4*(r + e)^2 + 4*l^2 - D^2) / (8*l*(r + e)) ); 
+%         as =  asin( (l*sin(pi / (2*n_s))) / (r + e(i)) );
+%         ac1 = acos( (4*(r + e(i))^2 + 4*l^2 - D^2) / (8*l*(r + e(i))) ); 
+%         ac2 = acos( (4*l^2 + D^2 - 4*(r + e(i))^2) / 4*l*D ); 
 
 %周长参数
-s_a0 = 2*pi*(R + l + n_s*r) + 2*pi*n_s*(r);      %第一阶段燃烧面初始边长
-    %s_a =  2*pi*(R + l + e) + 2*pi*n_s*(r + e);    %第一阶段燃烧周长公式
-    %s_b = 4*n_s*(r + e)*as + 2*pi*(l + R + e) ;  %第二阶段燃烧周长公式
-    %s_c = 2*n_s*(r + e)*(ac1 + 2*as - pi) + 2*pi*(l + R - r);  %第三阶段燃烧周长公式
+s_a0 = (4*pi*l*m_s) / (m_s + 1) + 2*pi*(R + n_s*r);      %第一阶段燃烧面初始边长
+    s_a =  (4*pi*l*m_s) / (m_s + 1) + 2*pi*((R + e) + n_s*(r + e));    %第一阶段燃烧周长公式
+    s_b = 2*n_s*(as1 + pi/2)*(r + e) + 2*n_s*(trr/2)*(l + R - r) ;  %第二阶段燃烧周长公式
+    s_c = 2*n_s*(r + e)*(ac2 + 2*as2 - pi) + 2*pi*R + 2*n_s*trr*(l - r);  %第三阶段燃烧周长公式
+    s_d = 2*n_s*(r + e)*(as2 - trd/2) + 2*pi*(R + e) + 2*n_s*trr*(l - r - e);  %第四阶段燃烧周长公式
 %通气面积参数
-Ap_a0 = 2*pi*l*r + n_s*pi*r^2 + pi*R^2;     %第一阶段初始通气面积
-    %Ap_a = 2*pi*l*(r + e) + n_s*pi*(r + e)^2 + pi*(R + e)^2;   %第一阶段通气面积
-    %Ap_b = 2*n_s*((r + e)^2)*as ...  %第二阶段通气面积
-        %+ 2*pi*l*(r + e) + pi*(R + e)^2 + n_s*((r + e)^2)*sin(2*as);
-    %Ap_c = pi*(Dr^2)/4 - pi*(R + e)^2 - n_s*Dr*(pi/2 -ac2) ...%第三阶段通气面积
-        %*(  Dr/2 - (r + e)*sin(as + pi / (2*n_s)) / sin(pi / (2*n_s))  );
+Ap_a0 = (4*pi*l*m_s*r) / (m_s + 1) + n_s*pi*r^2 + pi*R^2;     %第一阶段初始通气面积
+    Ap_a = (4*pi*l*m_s*(r + e)) / (m_s + 1) ... %第一阶段通气面积
+        + n_s*pi*(r + e)^2 + pi*(R + e)^2;   
+    Ap_b = n_s*(trr/2)*(R + e)^2 + n_s*(as1 + pi/2)*(r + e)^2 ...  %第二阶段通气面积
+        + n_s*D*ac1 - n_s*l*(D/2) + 2*n_s*l*(r + e)*trr;
+    %Ap_c = pi*(D^2)/4 - pi*(R + e)^2 - n_s*D*(pi/2 -ac2) ...%第三阶段通气面积
+        %*(  D/2 - (r + e)*sin(as + pi / (2*n_s)) / sin(pi / (2*n_s))  );
+    Ap_c = pi*(D^2)/4 - pi*(R + e)^2;  %第三阶段通气面积
+    Ap_d = pi*(D^2)/4 - pi*(R + e)^2;  %第四阶段通气面积
+    
+        
 
 %变量
 %计算用变量（已赋初值）
@@ -78,6 +94,9 @@ Ap = Ap_a0*ones(1,long);     %通气面积(m^2)
 Vg = Ap*Lp;     %自由体积(m^3)
 %循环变量
 i = 1;
+%状态变量
+SW = 0;     %阶段选择
+
 
 
 %数值计算
@@ -114,11 +133,11 @@ while e <= e_c
             + n_s*((r + e(i))^2)*sin(2*as) ;
     else
         as =  asin( (l*sin(pi / (2*n_s))) / (r + e(i)) );
-        ac1 = acos( (4*(r + e(i))^2 + 4*l^2 - Dr^2) / (8*l*(r + e(i))) ); 
-        ac2 = acos( (4*l^2 + Dr^2 - 4*(r + e(i))^2) / 4*l*Dr ); 
+        ac1 = acos( (4*(r + e(i))^2 + 4*l^2 - D^2) / (8*l*(r + e(i))) ); 
+        ac2 = acos( (4*l^2 + D^2 - 4*(r + e(i))^2) / 4*l*D ); 
         s(i) = 2*n_s*(r + e(i))*(ac1 + 2*as - pi) + 2*pi*(l + R - r);
-        Ap(i) = pi*(Dr^2)/4 - pi*(R + e(i))^2 - n_s*Dr*(pi/2 -ac2) ...
-            *( Dr/2 - (r + e(i))*sin(as + pi / (2*n_s)) / sin(pi / (2*n_s))  );
+        Ap(i) = pi*(D^2)/4 - pi*(R + e(i))^2 - n_s*D*(pi/2 -ac2) ...
+            *( D/2 - (r + e(i))*sin(as + pi / (2*n_s)) / sin(pi / (2*n_s))  );
     end
     
     Ab(i) = s(i)*Lp;
